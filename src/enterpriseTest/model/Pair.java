8b60f2102b13c5339802b271a613e56e047da701
João Lopes <joaopclopes@gmail.com>
2014-06-03 14:52:09 +0100

    Pair, which goes inside ACLMessages, must be serializable.

--- src/enterpriseTest/model/Pair.java
+++ src/enterpriseTest/model/Pair.java
@@ -1,28 +1,32 @@
 package enterpriseTest.model;
 
+import java.io.Serializable;
 
-public class Pair<T> implements Comparable<Pair<T>>{
 
+public class Pair<T> implements Comparable<Pair<T>>, Serializable {
+
+	private static final long serialVersionUID = -394110080537538203L;
 	public T key;
-	public double value;
+	public double value = 1;
 	
 	
 	public Pair(T key, double value) {
 		this.key = key;
 		this.value = value;
 	}
-
-
 	@Override
 	public int compareTo(Pair<T> pair) {
 		//  Return a negative integer, zero,
 		// or a positive integer as this object
 		// is less than, equal to, or greater
 		// than the specified object. 
-		
 		return
 			(value > pair.value) ?  1 :
 			(value < pair.value) ? -1 : 0;
-				
+	}
+	
+	@Override
+	public String toString() {
+		return key + " > " + value;
 	}
 }
